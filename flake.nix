974cd77d1141fddf792a19d67e662515f268beb5
Fovir <fovir@disroot.org>
2025-05-04 21:00:41 +0800

    fix: Correct spelling mistake.

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
             {
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
-              home-manager.users.fovir = imports ./home/fovir.nix;
+              home-manager.users.fovir = import ./home/fovir.nix;
             }
           ];
 
