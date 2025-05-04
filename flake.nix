181bf5b18c1b061db85bd6b511c69bdf7bd48bab
Fovir <fovir@disroot.org>
2025-05-04 20:55:50 +0800

    feat: Enable home manager.

--- flake.nix
+++ flake.nix
@@ -3,6 +3,10 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11"; # Use the stable version.
+
+    # Home Manager
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = { self, nixpkgs, ... }@inputs:
@@ -13,7 +17,16 @@
       mkHost = name:
         nixpkgs.lib.nixosSystem {
           inherit system;
-          modules = [ ./hosts/${name}.nix ];
+          modules = [
+            ./hosts/${name}.nix
+
+            inputs.home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.${name} = imports ./home/${name}.nix;
+            }
+          ];
 
           specialArgs = { inherit name inputs; };
         };
