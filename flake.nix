69327be07083e908412183e5b786b21782b3f824
Fovir <fovir@disroot.org>
2025-05-05 11:09:57 +0800

    fix: Correct spelling mistake.

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
             {
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
-              home-manager.users.${name} = imports ./home/${name}/${name}.nix;
+              home-manager.users.${name} = import ./home/${name}/${name}.nix;
             }
           ];
 
