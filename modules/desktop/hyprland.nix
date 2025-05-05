d43eedf63f2fb0b546c7d08a21232d4897f80465
Fovir <fovir@disroot.org>
2025-05-05 14:31:10 +0800

    feat: Bind `SUPER + Q` to execute `alacritty`.

--- modules/desktop/hyprland.nix
+++ modules/desktop/hyprland.nix
@@ -1,6 +1,10 @@
-{ pkgs, ... }: {
+{ config, pkgs, lib, ... }: {
   programs.hyprland = {
     enable = true;
     xwayland.enable = true;
+  };
+
+  wayland.windowManager.hyprland.settings = {
+    bind = [ "SUPER,Q,exec,alacritty" ];
   };
 }
