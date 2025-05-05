cfbd03d5aa0919fa9d2015c87f4c5c10b932ec4f
Fovir <fovir@disroot.org>
2025-05-05 22:26:46 +0800

    feat: Bind `ALT + SPACE` to run `wofi`.

--- modules/home/desktop/hyprland.nix
+++ modules/home/desktop/hyprland.nix
@@ -1,6 +1,11 @@
 {
   wayland.windowManager.hyprland = {
     enable = true;
-    settings = { bind = [ "SUPER,Q,exec,alacritty" ]; };
+    settings = {
+      bind = [
+        "SUPER,Q,exec,alacritty" # Run `alacritty`.
+        "ALT,SPACE,exec,wofi --show drun" # Run `wofi`.
+      ];
+    };
   };
 }
