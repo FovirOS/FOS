da631b8935f7f92da72cff1de163ad23c9f76472
Fovir <fovir@disroot.org>
2025-05-09 15:42:11 +0800

    feat: Update the import module.

--- home/qemu.nix
+++ home/qemu.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }: {
+{pkgs, ...}: {
   home.username = "qemu";
   home.homeDirectory = "/home/qemu";
 
@@ -7,7 +7,7 @@
   home.stateVersion = "24.11";
 
   imports = [
-    ../modules/home/develop # Import the develop softwares.
+    ../modules/home/development # Import the development softwares.
     ../modules/home/terminal # Import terminal applications.
     ../modules/home/desktop # Import the desktop.
   ];
