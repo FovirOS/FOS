1694e90b558de79001eb325a4a1b05de9491bdc9
Fovir <fovir@disroot.org>
2025-05-09 18:10:28 +0800

    feat: Configure `tabout` feature.

--- modules/home/development/nixvim/plugins.nix
+++ modules/home/development/nixvim/plugins.nix
@@ -26,12 +26,30 @@
         mapping = {
           "<Tab>" = ''
             function(fallback)
+              local col = vim.fn.col(".")
+              local line = vim.fn.getline(".")
+
+              local char = line:sub(col, col)
+              local prev = line:sub(col - 1, col - 1)
+
+              local jump_chars = { [")"]=true, ["}"]=true, ["]"]=true, ['"']=true, ["'"]=true, [">"]=true }
+
               if cmp.visible() then
                 cmp.confirm({select = true})
+              elseif jump_chars[char] and (prev:match("%s") or prev == "") then
+                local before = line:sub(1, col - 2)
+                local after = line:sub(col)
+
+                if (before:match("%b()") or before:match("%b[]") or before:match("%b{}")) then
+                  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("", true, true, true), "n", true)
+                else
+                  fallback()
+                end
               else
                 fallback()
               end
             end
+
           '';
         };
       };
