c47fac8c9366aa6c05ff4bc5354d918c6323555a
Fovir <fovir@disroot.org>
2025-05-09 16:36:51 +0800

    feat: Add `treesitter` plugins.

--- modules/home/development/nixvim/plugins.nix
+++ modules/home/development/nixvim/plugins.nix
@@ -3,5 +3,14 @@
     nvim-autopairs = {
       enable = true;
     };
+
+    treesitter = {
+      enable = true;
+
+      grammarPackages = with pkgs.vimPlugins.nvim-treesitter.builtGrammars; [
+        bash
+        nix
+      ];
+    };
   };
 }
