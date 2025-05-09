4b4805de9a4ad5d774041c9cc66d95cd95f04c81
Fovir <fovir@disroot.org>
2025-05-09 16:37:57 +0800

    feat: Add `cmp` plugin.

--- modules/home/development/nixvim/plugins.nix
+++ modules/home/development/nixvim/plugins.nix
@@ -12,5 +12,15 @@
         nix
       ];
     };
+
+    cmp = {
+      enable = true;
+      autoEnableSources = true;
+      settings.sources = [
+        {name = "nvim_lsp";}
+        {name = "path";}
+        {name = "buffer";}
+      ];
+    };
   };
 }
