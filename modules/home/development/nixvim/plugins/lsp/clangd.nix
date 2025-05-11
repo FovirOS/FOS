d79635d0bef38ecc98e6e3f03844d8a0e78d0772
Fovir <fovir@disroot.org>
2025-05-11 12:37:08 +0800

    feat: Disable header insertion of `clangd`.

--- modules/home/development/nixvim/plugins/lsp/clangd.nix
+++ modules/home/development/nixvim/plugins/lsp/clangd.nix
@@ -2,5 +2,10 @@
   plugins.lsp.servers.clangd = {
     enable = true;
     package = pkgs.llvmPackages_19.clang-tools;
+
+    cmd = [
+      "clangd"
+      "--header-insertion=never"
+    ];
   };
 }
