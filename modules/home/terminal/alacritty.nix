f0e105e02e0aa5ec59faec2fcc02a3b5d94c3aa2
Fovir <fovir@disroot.org>
2025-05-12 19:03:47 +0800

    feat: Set font for `alacritty`.

--- modules/home/terminal/alacritty.nix
+++ modules/home/terminal/alacritty.nix
@@ -1,1 +1,30 @@
-{ config, pkgs, ... }: { programs.alacritty = { enable = true; }; }
+{
+  config,
+  pkgs,
+  ...
+}: {
+  programs.alacritty = {
+    enable = true;
+
+    settings = {
+      font = {
+        normal = {
+          family = "JetBrainsMono Nerd Font Mono";
+          style = "Regular";
+        };
+
+        bold = {
+          family = "JetBrainsMono Nerd Font Mono";
+          style = "Bold";
+        };
+
+        italic = {
+          family = "JetBrainsMono Nerd Font Mono";
+          style = "Italic";
+        };
+
+        size = 12.0;
+      };
+    };
+  };
+}
