0c1a15cc1773ae969aecab8f0ca8701bd05d5db4
Fovir <fovir@disroot.org>
2025-05-12 17:35:59 +0800

    feat: Configure the font size.

--- modules/system/stylix.nix
+++ modules/system/stylix.nix
@@ -9,6 +9,13 @@
         name = "JetBrains Mono Nerd Font";
         package = pkgs.jetbrains-mono;
       };
+
+      sizes = {
+        applications = 16;
+        desktop = 14;
+        popups = 12;
+        terminal = 12;
+      };
     };
   };
 }
