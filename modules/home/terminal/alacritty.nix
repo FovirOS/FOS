3c1a000f40dd2d84535c137dec6feebb477f0b4c
Fovir <fovir@disroot.org>
2025-05-12 21:05:10 +0800

    feat: Configure `catppuccin` theme for `alacritty`.

--- modules/home/terminal/alacritty.nix
+++ modules/home/terminal/alacritty.nix
@@ -2,7 +2,18 @@
   config,
   pkgs,
   ...
-}: {
+}: let
+  catppuccinTheme = pkgs.fetchFromGitHub {
+    owner = "catppuccin";
+    repo = "alacritty";
+    rev = "main";
+    sha256 = "H8bouVCS46h0DgQ+oYY8JitahQDj0V9p2cOoD4cQX+Q=";
+  };
+in {
+  home.file.".config/alacritty/catppuccin-mocha.toml" = {
+    source = "${catppuccinTheme}/catppuccin-mocha.toml";
+  };
+
   programs.alacritty = {
     enable = true;
 
@@ -24,6 +35,10 @@
         };
 
         size = 12.0;
+      };
+
+      general = {
+        import = ["./catppuccin-mocha.toml"];
       };
     };
   };
