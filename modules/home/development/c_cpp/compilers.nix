5f9daa2d738d429eff3c8d554a8550505557d94a
Fovir <fovir@disroot.org>
2025-05-13 11:37:08 +0800

    feat: Update the `.clang-tidy` file.

--- modules/home/development/c_cpp/compilers.nix
+++ modules/home/development/c_cpp/compilers.nix
@@ -9,7 +9,7 @@
   ];
 
   home.file.".clang-tidy".text = ''
-    Checks: '*, -llvm*, -modernize-use-trailing-return-type, -readability-braces-around-statements, -performance-unroll-loops'
+    Checks: '*, -llvm*, -modernize-use-trailing-return-type, -readability-braces-around-statements, -altera*'
   '';
 
   home.file.".clang-format".text = ''
