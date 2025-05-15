1194bbab03c9e1d5ae9f8e34a0657dd2e7ac8b95
Fovir <fovir@disroot.org>
2025-05-15 22:14:11 +0800

    feat: Configure key binds.

--- modules/home/desktop/hyprland.nix
+++ modules/home/desktop/hyprland.nix
@@ -3,8 +3,27 @@
     enable = true;
     settings = {
       bind = [
-        "SUPER,Q,exec,alacritty" # Run `alacritty`.
+        "SUPER,Q,killactive" # Kill current process.
         "ALT,SPACE,exec,wofi --show drun" # Run `wofi`.
+
+        "SUPER,SPACE,togglefloating"
+
+        # Move focus.
+        "SUPER,H,movefocus,l"
+        "SUPER,J,movefocus,u"
+        "SUPER,K,movefocus,d"
+        "SUPER,l,movefocus,r"
+
+        # Switch workspace
+        "SUPER,1,workspace,1"
+        "SUPER,2,workspace,2"
+        "SUPER,3,workspace,3"
+        "SUPER,4,workspace,4"
+        "SUPER,5,workspace,5"
+        "SUPER,6,workspace,6"
+        "SUPER,7,workspace,7"
+        "SUPER,8,workspace,8"
+        "SUPER,9,workspace,9"
       ];
 
       input = {
