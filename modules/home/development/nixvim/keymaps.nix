99e999a39685f9d414811c6c5e2a8e4904e0b72f
Fovir <fovir@disroot.org>
2025-05-15 12:40:53 +0800

    chore: Add descriptions for keymaps.

--- modules/home/development/nixvim/keymaps.nix
+++ modules/home/development/nixvim/keymaps.nix
@@ -4,18 +4,21 @@
       action = "<C-w>";
       key = "<C-BS>";
       mode = "i";
+      options.desc = "Delete a Word";
     }
 
     {
       action = "<C-w>c";
       key = "<leader>w";
       mode = "n";
+      options.desc = "Close Current Buffer";
     }
 
     {
       action = "<C-w>v";
       key = "<leader>v";
       mode = "n";
+      options.desc = "Split Tab Vertically";
     }
   ];
 }
--- modules/home/development/nixvim/plugins/lspsaga.nix
+++ modules/home/development/nixvim/plugins/lspsaga.nix
@@ -11,18 +11,21 @@
       key = "<C-.>";
       mode = "n";
       action = "<cmd>Lspsaga code_action<CR>";
+      options.desc = "Code Action";
     }
 
     {
       key = "<F2>";
       mode = "n";
       action = "<cmd>Lspsaga rename<CR>";
+      options.desc = "Rename";
     }
 
     {
       key = "<F3>";
       mode = "n";
       action = "<cmd>Lspsaga peek_definition<CR>";
+      options.desc = "Peek Definition";
     }
   ];
 }
