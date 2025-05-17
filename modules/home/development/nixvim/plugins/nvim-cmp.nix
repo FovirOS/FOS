f2a51ed07745426e229a4669b33317570b431173
Fovir <fovir@disroot.org>
2025-05-17 17:11:55 +0800

    test: Try to enable `luasnip` for `c` and `cpp`.

--- modules/home/development/nixvim/plugins/nvim-cmp.nix
+++ modules/home/development/nixvim/plugins/nvim-cmp.nix
@@ -15,6 +15,7 @@
         sources = [
           {name = "nvim_lsp";}
           {name = "path";}
+          {name = "luasnip";}
         ];
       };
 
@@ -22,6 +23,7 @@
         sources = [
           {name = "nvim_lsp";}
           {name = "path";}
+          {name = "luasnip";}
         ];
       };
     };
@@ -38,6 +40,7 @@
         "require('cmp.config.compare').exact"
         "require('cmp.config.compare').order"
       ];
+
       mapping = {
         "<Down>" = "cmp.mapping.select_next_item()";
         "<Up>" = "cmp.mapping.select_prev_item()";
