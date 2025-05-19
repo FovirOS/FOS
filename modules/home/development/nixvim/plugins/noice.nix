770bbac752d0a6d40b0ffdd121e917bdc43fa630
Fovir <fovir@disroot.org>
2025-05-19 22:16:25 +0800

    feat: Configure the sytle of `noice`.

--- modules/home/development/nixvim/plugins/noice.nix
+++ modules/home/development/nixvim/plugins/noice.nix
@@ -1,5 +1,15 @@
 {...}: {
   plugins.noice = {
     enable = true;
+
+    settings = {
+      lsp.enabled = true;
+
+      presets = {
+        command_palette = true;
+      };
+    };
   };
+
+  plugins.notify.enable = true;
 }
