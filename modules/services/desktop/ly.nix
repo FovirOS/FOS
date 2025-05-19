2551967892953d18185997eedfb47d44ee1c5317
Fovir <fovir@disroot.org>
2025-05-19 22:33:34 +0800

    feat: Configure `ly`.

--- modules/services/desktop/ly.nix
+++ modules/services/desktop/ly.nix
@@ -1,5 +1,19 @@
 {pkgs, ...}: {
   services.displayManager.ly = {
     enable = true;
+
+    settings = {
+      # Set `vim` mode.
+      vi_mode = true;
+      vi_default_mode = "insert";
+
+      allow_empty_password = false;
+      auth_fails = 5;
+
+      animation = "CMatrix";
+
+      # Hide password.
+      asterisk = "";
+    };
   };
 }
