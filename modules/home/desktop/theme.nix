27e75feaf375c03886139231031a42ad8c16ce88
Fovir <fovir@disroot.org>
2025-05-20 20:24:38 +0800

    feat: Configure the theme for desktop.

---
+++ modules/home/desktop/theme.nix
@@ -0,0 +1,15 @@
+{pkgs, ...}: {
+  gtk = {
+    gtk3.extraConfig = {
+      gtk-application-prefer-dark-theme = true;
+    };
+
+    gtk4.extraConfig = {
+      gtk-application-prefer-dark-theme = true;
+    };
+    iconTheme = {
+      name = "Beauty Line";
+      package = pkgs.beauty-line-icon-theme;
+    };
+  };
+}
