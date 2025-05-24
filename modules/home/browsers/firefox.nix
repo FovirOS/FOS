637661b417ac8cfd0ac09af153cf71748529fc75
Fovir <fovir@disroot.org>
2025-05-24 22:18:54 +0800

    feat: Configure the vertical tabs for `firefox`.

--- modules/home/browsers/firefox.nix
+++ modules/home/browsers/firefox.nix
@@ -3,5 +3,19 @@
     enable = true;
 
     languagePacks = ["en-US"];
+
+    policies = {
+      DisableAppUpdate = true;
+      DisableFirefoxAccounts = true;
+      DisableFirefoxScreenshots = true;
+      DisableFirefoxStudies = true;
+    };
+
+    preferences = {
+      "sidebar.verticalTabs" = true;
+      "sidebar.visibility" = "expand-on-hover";
+      "sidebar.expandOnHover" = true;
+      "sidebar.main.tools" = "history,bookmarks";
+    };
   };
 }
