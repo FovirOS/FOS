07796df122ed407d0a30876cdb0dc88a32c0716f
Fovir <fovir@disroot.org>
2025-05-25 11:23:58 +0800

    feat: Add the privacy configuration.

--- modules/home/browsers/firefox.nix
+++ modules/home/browsers/firefox.nix
@@ -5,10 +5,22 @@
     languagePacks = ["en-US"];
 
     policies = {
+      DisableAccounts = true;
       DisableAppUpdate = true;
       DisableFirefoxAccounts = true;
       DisableFirefoxScreenshots = true;
       DisableFirefoxStudies = true;
+      DisablePocket = true;
+      DisableTelemetry = true;
+      DisplayBookmarksToolbar = "newtab";
+      DisplayMenuBar = "default-off";
+      DontCheckDefaultBrowser = true;
+      EnableTrackingProtection = {
+        Cryptomining = true;
+        Fingerprinting = true;
+        Locked = true;
+        Value = true;
+      };
 
       Preferences = {
       };
