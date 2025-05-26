c301997e885a60dafb39efefe83aeed1cc191b95
Fovir <fovir@disroot.org>
2025-05-26 16:34:07 +0800

    chore: Sync the flake configuration.

--- flake.nix
+++ flake.nix
@@ -2,39 +2,66 @@
   description = "FOS Flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11"; # Use the stable version.
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; # Use the unstable version.
 
     # Home Manager
-    home-manager.url = "github:nix-community/home-manager/release-24.11";
+    home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
+
+    disko.url = "github:nix-community/disko/latest";
+    disko.inputs.nixpkgs.follows = "nixpkgs";
+
+    nixvim = {
+      url = "github:nix-community/nixvim";
+    };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs:
-    let
-      system = "x86_64-linux";
-      hosts = [ "laptop" "qemu" ];
+  outputs = {
+    self,
+    nixpkgs,
+    home-manager,
+    nixvim,
+    chaotic,
+    disko,
+    ...
+  } @ inputs: let
+    system = "x86_64-linux";
+    hosts = ["laptop" "qemu"];
+    pkgs = import nixpkgs {inherit system;};
 
-      mkHost = name:
-        nixpkgs.lib.nixosSystem {
-          inherit system;
-          modules = [
-            ./hosts/${name}.nix
+    mkHost = name:
+      nixpkgs.lib.nixosSystem {
+        inherit system;
 
-            inputs.home-manager.nixosModules.home-manager
-            {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.users.${name} = import ./home/${name}.nix;
-            }
-          ];
+        specialArgs = {inherit name inputs;};
 
-          specialArgs = { inherit name inputs; };
-        };
-    in {
-      nixosConfigurations = builtins.listToAttrs (builtins.map (name: {
+        modules = [
+          ./hosts/${name}.nix
+
+          chaotic.nixosModules.default
+          disko.nixosModules.disko
+
+          inputs.home-manager.nixosModules.home-manager
+          {
+            home-manager.extraSpecialArgs = {inherit inputs;};
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.${name} = {
+              imports = [
+                ./home/${name}.nix
+                nixvim.homeManagerModules.nixvim
+              ];
+            };
+          }
+        ];
+      };
+  in {
+    nixosConfigurations = builtins.listToAttrs (builtins.map (name: {
         inherit name;
         value = mkHost name;
-      }) hosts);
-    };
-
+      })
+      hosts);
+  };
 }
