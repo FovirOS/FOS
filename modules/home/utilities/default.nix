f77c6060c17a912f41b87df8e34a473829d6b1cf
Fovir <fovir@disroot.org>
2025-05-29 20:23:01 +0800

    feat: Import the `fcitx5`.

--- modules/home/utilities/default.nix
+++ modules/home/utilities/default.nix
@@ -2,5 +2,6 @@
   imports = [
     ./keepassxc.nix
     ./thunderbird.nix
+    ./fcitx5.nix
   ];
 }
--- modules/home/utilities/fcitx5.nix
+++ modules/home/utilities/fcitx5.nix
@@ -14,14 +14,14 @@
     };
   };
 
-  i18n.inputMethod = {
-    type = "fcitx5";
-    enable = true;
+  # i18n.inputMethod = {
+  #   type = "fcitx5";
+  #   enable = true;
 
-    fcitx5.addons = with pkgs; [
-      fcitx5-gtk
-      fcitx5-rime
-      libsForQt5.fcitx5-qt
-    ];
-  };
+  #   fcitx5.addons = with pkgs; [
+  #     fcitx5-gtk
+  #     fcitx5-rime
+  #     libsForQt5.fcitx5-qt
+  #   ];
+  # };
 }
