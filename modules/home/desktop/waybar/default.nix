3a62e5c9d0621e4ef6f09fe58d65131d622b52ab
Fovir <fovir@disroot.org>
2025-05-31 17:06:44 +0800

    fix: Import the dot files.

--- modules/home/desktop/waybar/default.nix
+++ modules/home/desktop/waybar/default.nix
@@ -2,4 +2,7 @@
   programs.waybar = {
     enable = true;
   };
+
+  home.file.".config/waybar/config.jsonc".text = builtins.readFile ./config.jsonc;
+  home.file.".config/waybar/style.css".text = builtins.readFile ./style.css;
 }
