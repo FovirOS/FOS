08faa15667a825790d60cc08bd3e4f941e941699
Fovir <fovir@disroot.org>
2025-05-31 21:32:23 +0800

    feat: Configure the QT theme.

--- modules/home/desktop/qt/default.nix
+++ modules/home/desktop/qt/default.nix
@@ -9,4 +9,7 @@
     enable = true;
     style.name = "kvantum";
   };
+
+  home.file.".config/qt5ct/qt5ct.conf".text = builtins.readFile ./qt5ct.conf;
+  home.file.".config/qt6ct/qt6ct.conf".text = builtins.readFile ./qt6ct.conf;
 }
