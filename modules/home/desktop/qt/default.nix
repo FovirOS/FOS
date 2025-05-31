1ba5822bd89ec619735f10798ee934d8e68b016b
Fovir <fovir@disroot.org>
2025-05-31 21:50:16 +0800

    feat: Add `catppuccin-mocha-lavender` theme for Qt.

--- modules/home/desktop/qt/default.nix
+++ modules/home/desktop/qt/default.nix
@@ -12,6 +12,8 @@
 
   home.file.".config/qt5ct/qt5ct.conf".text = builtins.readFile ./qt5ct.conf;
   home.file.".config/qt6ct/qt6ct.conf".text = builtins.readFile ./qt6ct.conf;
+  home.file.".config/qt5ct/catppuccin-mocha-lavender.conf".text = builtins.readFile ./catppuccin-mocha-lavender.conf;
+  home.file.".config/qt6ct/catppuccin-mocha-lavender.conf".text = builtins.readFile ./catppuccin-mocha-lavender.conf;
 
   home.file.".config/Kvantum/kvantum.kvconfig".text = builtins.readFile ./kvantum.kvconfig;
   home.file.".config/Kvantum/catppuccin-mocha-lavender/catppuccin-mocha-lavender.kvconfig".text = builtins.readFile "./catppuccin-mocha-lavender/catppuccin-mocha-lavender.kvconfig";
