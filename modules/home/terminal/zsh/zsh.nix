c4b580e5d594a3ec4b6600cb793df8b6e1fd7a79
Fovir <fovir@disroot.org>
2025-06-01 12:19:49 +0800

    feat: Alias `open`.

--- modules/home/terminal/zsh/zsh.nix
+++ modules/home/terminal/zsh/zsh.nix
@@ -13,10 +13,12 @@
     syntaxHighlighting.enable = true;
 
     shellAliases = {
-      ls = "eza -alF --color=always --group --group-directories-first --icons=always"; # preferred listing
-      la = "eza -a --color=always --group-directories-first --icons=always"; # all files and dirs
-      ll = "eza -l --color=always --group-directories-first --icons=always"; # long format
-      lt = "eza -aT --color=always --group-directories-first --icons=always"; # tree listing
+      ls = "eza -alF --color=always --group --group-directories-first --icons=always"; # Preferred listing.
+      la = "eza -a --color=always --group-directories-first --icons=always"; # All files and dirs.
+      ll = "eza -l --color=always --group-directories-first --icons=always"; # Long format.
+      lt = "eza -aT --color=always --group-directories-first --icons=always"; # Tree listing.
+
+      open = "xdg-open"; # Open.
     };
 
     initContent = ''
