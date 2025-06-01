b4f7b3a3410f9300c123737f0357dfe657758da1
Fovir <fovir@disroot.org>
2025-06-01 12:16:22 +0800

    style: Remove useless comments.

--- modules/home/terminal/zsh/zsh.nix
+++ modules/home/terminal/zsh/zsh.nix
@@ -15,18 +15,6 @@
     shellAliases = {
     };
 
-    # initExtraFirst = ''
-    #   source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
-    #   source $HOME/.p10k.zsh
-    # '';
-
-    # initExtra = ''
-    #   set -o vi
-    #   bindkey -M viins '^H' backward-kill-word
-
-    #   fastfetch
-    # '';
-
     initContent = ''
       source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
       source $HOME/.p10k.zsh
