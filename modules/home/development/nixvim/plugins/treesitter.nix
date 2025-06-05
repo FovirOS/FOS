f7d38b22f112c90b195217e391cb66afe055ff0b
Fovir <fovir@disroot.org>
2025-06-05 18:59:49 +0800

    fix: Fix the fold method and expr.

--- modules/home/development/nixvim/plugins/treesitter.nix
+++ modules/home/development/nixvim/plugins/treesitter.nix
@@ -18,7 +18,7 @@
 
   extraConfigLua = ''
     vim.o.foldmethod = "expr"
-    vim.o.foldexpr = "nvim_treesitter#foldexpr()"
+    vim.o.foldexpr = "v:lua.vim.treesitter.foldexpr()"
     vim.o.foldenable = true
     vim.o.foldlevel = 99
     vim.o.foldlevelstart = 99
