f09cf8fdc6c53efe7503d20fd021b751978ca4a1
Fovir <fovir@disroot.org>
2025-06-05 18:41:33 +0800

    feat: Set the fold.

--- modules/home/development/nixvim/plugins/treesitter.nix
+++ modules/home/development/nixvim/plugins/treesitter.nix
@@ -12,4 +12,12 @@
 
     grammarPackages = config.plugins.treesitter.package.passthru.allGrammars;
   };
+
+  extraConfigLua = ''
+    vim.o.foldmethod = "indent"
+    vim.o.foldexpr = "nvim_treesitter#foldexpr()"
+    vim.o.foldenable = true
+    vim.o.foldlevel = 99
+    vim.o.foldlevelstart = 99
+  '';
 }
