76f39a21e76b62ec2d85fb606acf2f3beccbe524
Fovir <fovir@disroot.org>
2025-06-08 01:39:38 +0800

    feat: Add `aerial`.

---
+++ modules/home/development/nixvim/plugins/aerial.nix
@@ -0,0 +1,13 @@
+{...}: {
+  plugins.aerial = {
+    enable = true;
+
+    settings = {
+      open_automatic = true;
+
+      layout = {
+        default_direction = "right";
+      };
+    };
+  };
+}
