2544f49692131ccbfccd42c144d544ff55c51464
Fovir <fovir@disroot.org>
2025-07-06 17:36:10 +0800

    feat: Ignore the shell program check.

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -45,6 +45,7 @@
       extraGroups = ["wheel" "networkmanager"];
       hashedPassword = "$6$Z3tVvcb3O79M9wVa$eQsKKb4DbQMqolBLpy8bpNr1XFdl.lg4ek649YfWQ4nxW.EOGUdvKV4txOQH/RfozyPJwMu2o335K./TEF/gg.";
       shell = pkgs.zsh;
+      ignoreShellProgramCheck = true;
       homeMode = "755";
     };
 
