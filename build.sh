671ecef002ff3931af7582f9f7455313a7e3f790
Fovir <fovir@disroot.org>
2025-07-07 11:03:41 +0800

    fix: Reset the build script.

--- build.sh
+++ build.sh
@@ -2,7 +2,24 @@
 
 set -euo pipefail
 
+SOURCE_DIR=".";
+TARGET_DIR="/etc/nixos";
+
+echo "Run rsync..."
+
+sudo rsync -av --delete \
+    --exclude '.git' \
+    --exclude 'build.sh' \
+    "$SOURCE_DIR/" "$TARGET_DIR"
+
 echo "Run nixos-rebuild switch..."
-sudo nixos-rebuild switch --flake .#laptop;
+sudo nixos-rebuild switch --flake "$TARGET_DIR"#laptop;
+
+echo "Sync hardware-configuration.nix and flake.lock..."
+
+rsync -av \
+    "$TARGET_DIR/hardware-configuration.nix" \
+    "$TARGET_DIR/flake.lock" \
+    "$SOURCE_DIR/"
 
 echo "Done!"
