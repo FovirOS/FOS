e531db15eb1db51b339d132999f66bd7d3c95206
Fovir <fovir@disroot.org>
2025-07-07 10:29:35 +0800

    feat: Update the build script.

--- build.sh
+++ build.sh
@@ -2,24 +2,7 @@
 
 set -euo pipefail
 
-SOURCE_DIR=".";
-TARGET_DIR="/etc/nixos";
-
-echo "Run rsync..."
-
-sudo rsync -av --delete \
-    --exclude '.git' \
-    --exclude 'build.sh' \
-    "$SOURCE_DIR/" "$TARGET_DIR"
-
 echo "Run nixos-rebuild switch..."
-sudo nixos-rebuild switch --flake "$TARGET_DIR"#laptop;
-
-echo "Sync hardware-configuration.nix and flake.lock..."
-
-rsync -av \
-    "$TARGET_DIR/hardware-configuration.nix" \
-    "$TARGET_DIR/flake.lock" \
-    "$SOURCE_DIR/"
+sudo nixos-rebuild switch --flake .#laptop;
 
 echo "Done!"
