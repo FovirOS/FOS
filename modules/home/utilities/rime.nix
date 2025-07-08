c887bf721f900b0f5e78f6a01e874d6695e46f38
Fovir <fovir@disroot.org>
2025-07-08 15:19:04 +0800

    fix: Use the correct hash value.

--- modules/home/utilities/rime.nix
+++ modules/home/utilities/rime.nix
@@ -3,7 +3,7 @@
     force = true;
     recursive = true;
     source = pkgs.fetchFromGitHub {
-      hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+      hash = "sha256-yCVcTc8qitar5JJfVTH4xNJMTPgx/NsRMoTxVm5PVrY=";
       owner = "FovirOS";
       repo = "rime";
       rev = "7acdee60d09602383b6299d1bdaaba03f0a57869";
