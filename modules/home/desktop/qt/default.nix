d653e2bf62dfdabfef05ef8eeece2252ed0515e9
Fovir <fovir@disroot.org>
2025-07-18 22:17:47 +0800

    refactor: Replace `builtins.readFile` with `<name>.source`.

--- modules/home/desktop/qt/default.nix
+++ modules/home/desktop/qt/default.nix
@@ -14,13 +14,13 @@
     style.name = "kvantum";
   };
 
-  home.file."${config.xdg.configHome}/qt5ct/qt5ct.conf".text = builtins.readFile ./qt5ct.conf;
-  home.file."${config.xdg.configHome}/qt6ct/qt6ct.conf".text = builtins.readFile ./qt6ct.conf;
-  home.file."${config.xdg.configHome}/qt5ct/catppuccin-mocha-lavender.conf".text = builtins.readFile ./catppuccin-mocha-lavender.conf;
-  home.file."${config.xdg.configHome}/qt6ct/catppuccin-mocha-lavender.conf".text = builtins.readFile ./catppuccin-mocha-lavender.conf;
+  home.file."${config.xdg.configHome}/qt5ct/qt5ct.conf".source = ./qt5ct.conf;
+  home.file."${config.xdg.configHome}/qt6ct/qt6ct.conf".source = ./qt6ct.conf;
+  home.file."${config.xdg.configHome}/qt5ct/catppuccin-mocha-lavender.conf".source = ./catppuccin-mocha-lavender.conf;
+  home.file."${config.xdg.configHome}/qt6ct/catppuccin-mocha-lavender.conf".source = ./catppuccin-mocha-lavender.conf;
 
-  home.file."${config.xdg.configHome}/Kvantum/kvantum.kvconfig".text = builtins.readFile ./kvantum.kvconfig;
-  home.file."${config.xdg.configHome}/Kvantum/catppuccin-mocha-lavender/catppuccin-mocha-lavender.kvconfig".text = builtins.readFile ./catppuccin-mocha-lavender/catppuccin-mocha-lavender.kvconfig;
+  home.file."${config.xdg.configHome}/Kvantum/kvantum.kvconfig".source = ./kvantum.kvconfig;
+  home.file."${config.xdg.configHome}/Kvantum/catppuccin-mocha-lavender/catppuccin-mocha-lavender.kvconfig".source = ./catppuccin-mocha-lavender/catppuccin-mocha-lavender.kvconfig;
 
   home.file."${config.xdg.configHome}/Kvantum/catppuccin-mocha-lavender/catppuccin-mocha-lavender.svg" = {
     source = ./catppuccin-mocha-lavender/catppuccin-mocha-lavender.svg;
