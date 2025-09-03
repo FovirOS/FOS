cc4bd0a2bc8cd882f8594151c5228c63ec7ade34
Fovir <fovir@disroot.org>
2025-09-03 12:41:02 +0800

    feat: Use noto sans and noto serif.

--- modules/desktop/fonts.nix
+++ modules/desktop/fonts.nix
@@ -1,18 +1,16 @@
-{
-  config,
-  pkgs,
-  ...
-}: {
+{pkgs, ...}: {
   fonts = {
     packages = with pkgs; [
       nerd-fonts.jetbrains-mono
       lxgw-wenkai
+      noto-fonts-cjk-sans
+      noto-fonts-cjk-serif
     ];
 
     fontconfig = {
       defaultFonts = {
-        serif = ["JetBrainsMono Nerd Font Mono" "LXGW WenKai Mono"];
-        sansSerif = ["JetBrainsMono Nerd Font Mono" "LXGW WenKai Mono"];
+        serif = ["Noto Serif CJK SC"];
+        sansSerif = ["Noto Sans CJK SC"];
         monospace = ["JetBrainsMono Nerd Font Mono" "LXGW WenKai Mono"];
       };
     };
