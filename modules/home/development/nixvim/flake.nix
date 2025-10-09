16fef5a12b84da6a646b3ebe0116908f022882ba
Fovir <fovir@disroot.org>
2025-10-09 23:58:14 +0800

    feat: Convert submodule `nixvim` to regular file.

---
+++ modules/home/development/nixvim/flake.nix
@@ -0,0 +1,31 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; # Use the unstable version.
+
+    flake-utils.url = "github:numtide/flake-utils";
+    nixvim = {
+      url = "github:Fovir-GitHub/nixvim";
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    nixvim,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+      nixvim' = nixvim.legacyPackages.${system};
+
+      my-nvim = nixvim'.makeNixvim {
+        # inherit pkgs;
+        imports = [
+          ./profile_install.nix
+        ];
+      };
+    in {
+      packages.default = my-nvim;
+    });
+}
---
+++ modules/home/development/nixvim/plugins/lspsaga.nix
@@ -0,0 +1,48 @@
+{...}: {
+  plugins.lspsaga = {
+    enable = true;
+
+    settings = {
+      code_action.keys.quit = "<esc>";
+      rename.keys.quit = "<esc>";
+      lightbulb.enable = false;
+    };
+  };
+
+  keymaps = [
+    {
+      key = "<C-.>";
+      mode = "n";
+      action = "<cmd>Lspsaga code_action<CR>";
+      options.desc = "Code Action";
+    }
+
+    {
+      key = "<leader>2";
+      mode = "n";
+      action = "<cmd>Lspsaga rename<CR>";
+      options.desc = "Rename";
+    }
+
+    {
+      key = "<F2>";
+      mode = "n";
+      action = "<cmd>Lspsaga rename<CR>";
+      options.desc = "Rename";
+    }
+
+    {
+      key = "<leader>3";
+      mode = "n";
+      action = "<cmd>Lspsaga peek_definition<CR>";
+      options.desc = "Peek Definition";
+    }
+
+    {
+      key = "<F3>";
+      mode = "n";
+      action = "<cmd>Lspsaga peek_definition<CR>";
+      options.desc = "Peek Definition";
+    }
+  ];
+}
