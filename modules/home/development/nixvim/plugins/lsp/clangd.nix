c591926b8fe5f0492c0691784ba4cc31f7bb6cdc
Fovir <fovir@disroot.org>
2025-10-09 23:58:14 +0800

    feat: Convert submodule `nixvim` to regular file.

---
+++ modules/home/development/nixvim/plugins/lsp/clangd.nix
@@ -0,0 +1,11 @@
+{pkgs, ...}: {
+  plugins.lsp.servers.clangd = {
+    enable = true;
+    package = pkgs.llvmPackages_20.clang-tools;
+
+    cmd = [
+      "clangd"
+      "--clang-tidy"
+    ];
+  };
+}
---
+++ modules/home/development/nixvim/plugins/lsp/default.nix
@@ -0,0 +1,24 @@
+{...}: {
+  imports = [
+    ./sqls.nix
+    ./jdtls.nix
+    ./lemminx.nix
+    ./just.nix
+    ./tailwindcss.nix
+    ./ts_ls.nix
+    ./eslint.nix
+    ./jsonls.nix
+    ./cssls.nix
+    ./pyright.nix
+    ./clangd.nix
+    ./ltex.nix
+    ./gopls.nix
+    ./nil.nix
+    ./html.nix
+    ./yamlls.nix
+  ];
+
+  plugins.lsp = {
+    enable = true;
+  };
+}
